f0f76a0f13bd32d48d098d04b336421a0d51891a
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-02-21 17:28:37 +0100

    [add] click function

--- JS/script.js
+++ JS/script.js
@@ -6,9 +6,13 @@ const rate = 0.21;
 const youngDiscount = 20;
 const seniorDiscount = 40;
 let price;   
-let finalPrice;
-let isValidData = true;
+let ticketName;
+let txtTicketName = "";
+const isValidData = true;
 
+// BOTTONI
+const btnGen = document.querySelector('.btn-gen');
+const btnReset = document.querySelector('.btn-res');
 
 
 // CONTROLLO VALIDITÀ DEI DATI
@@ -22,9 +26,21 @@ if(age.value === '1'){
     price *= 1 -(youngDiscount / 100);
 }else if(age.value === '2'){
     price = price;
-} else{
+}else{
     price *= 1 -(seniorDiscount / 100);
 }
+price = price.toFixed(2);
+
+// FUNZIONI
+ticketName = document.getElementById('ticket-name');
+ticketName.innerHTML = txtTicketName;
+
+btnGen.addEventListener('click', function(){
+    // neutralizzo l'action del form
+    event.preventDefault();
+    // aggiungo il nome nel ticket
+    ticketName.innerHTML = name.value;
+})
 
 
 
@@ -32,4 +48,3 @@ console.log(price);
 console.log(name.value);
 console.log(km.value);
 console.log(age.value);
-console.log(finalPrice);
